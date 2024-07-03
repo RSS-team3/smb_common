55fc2d09258812fe9c3b8af943d34a625ea3c12f
Zeno Hamers <zeno.hamers@gmail.com>
2024-07-03 12:03:10 +0000

    filter_real_detections function

--- post_processiong_detection/post_processing.py
+++ post_processiong_detection/post_processing.py
@@ -17,29 +17,56 @@ def object_detections_to_world_frame():
     pass
 
 
+def filter_real_detections(detected_objects:list, confidence_threshold:float, num_detections_threshold:int, object_radius:float):
+    """ Function that filters out real object detections and outputs thea averge location of the detected objec
+    
+    Args:
+        detected_objects (list): List of tuples containing the detected object name, object location (x, y, z), and object confidence score.
+        confidence_threshold (float): Threshold for filtering out low confidence detections.
+        num_detections_threshold (int): Minimum number of detections to consider an object as real.
+        object_radius (float): Radius to consider for merging nearby detections.
+    
+    Returns:
+        list: List of tuples containing the real detected object name and its average location (x, y, z).
+    """
 
-def filter_real_detections(detected_objects, confidence_scores, confidence_threshold, num_detections_threshold):
+    # Filter out low confidence detections
+    confident_detected_objects = [obj for obj in detected_objects if obj[2] > confidence_threshold]
+    if not confident_detected_objects:
+        return []
+    
     same_detections = 0
-    real_detection_indexes = []
+    real_detection_indices = []
     i = 0
     
-    # filter out low confidence detections
-    np.where(confidence_scores < confidence_threshold)
+    # set base object location (first detection)
+    base_object_location = confident_detected_objects[i][1]
 
-    for j in range(len(detected_objects)):
-        if detected_objects[i] == detected_objects[j]:
+    # Identify the real detections and add their indices to a list
+    # Calculate the sequence length (# of sequential detections of the same object around the same location)
+    for j in range(len(confident_detected_objects)):
+
+        # Calculate the distance between the base object (detected first) and the current object
+        base_object_distance = np.linalg.norm(np.array(base_object_location) - np.array(confident_detected_objects[j][1]))
+
+        if confident_detected_objects[i][2] == confident_detected_objects[j][2] and base_object_distance < object_radius:
             same_detections += 1
 
-        elif detected_objects[i] != detected_objects[j]:
+        else:
             if same_detections >= num_detections_threshold:
-                real_detection_indexes.extend(range(i, j))
+                real_detection_indices.extend(range(i, j))
+
+            # update variables
             i = j
             same_detections = 0
+            
+            # update base object location
+            base_object_location = confident_detected_objects[i][1]
 
-    return real_detection_indexes
+    return confident_detected_objects[real_detection_indices]
+
+            
     
-
-def 
 
 
 
