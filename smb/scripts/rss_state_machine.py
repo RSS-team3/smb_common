3fcef9433d318072bd3eed4948940f6fc26a6f5e
Andrew <andrew.wong.0101@gmail.com>
2024-07-04 07:30:46 +0000

    Setting to 15 minutes for TARE planner

--- smb/scripts/rss_state_machine.py
+++ smb/scripts/rss_state_machine.py
@@ -11,7 +11,8 @@ class RssStateMachine:
         # Start with idle mode, receives nothing and do not publish anything
         self.state = "tare"
         self.rate = rospy.Rate(10)  # 10hz
-        self.tare_timeout_thres = rospy.Duration.from_sec(20)  # seconds
+        self.tare_timeout_thres = rospy.Duration.from_sec(
+            900)  # in seconds (15 minutes)
 
         self.planner_state_publisher = rospy.Publisher(
             "planner_state", String, queue_size=10)
